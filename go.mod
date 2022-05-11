521ba083f353b1e511294729f82e7af95870a9e2
Francisco Rossi <65981860+FranRossi@users.noreply.github.com>
2022-05-11 18:14:40 -0300

    Adds connection to mongo database running in local container

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module 239850_221025_219401
 go 1.18
 
 require (
+	go.mongodb.org/mongo-driver v1.9.1
+	google.golang.org/grpc v1.46.0
+	google.golang.org/protobuf v1.28.0
 	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220505152158-f39f71e6c8f3 // indirect
-	google.golang.org/grpc v1.46.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import api_voter "239850_221025_219401/api-voter"
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
 
 func main() {
-	api_voter.Connection()
+	//
+	//api_voter.Connection()
+	const uri = "mongodb://localhost:27017"
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases, "anduvo")
 }
