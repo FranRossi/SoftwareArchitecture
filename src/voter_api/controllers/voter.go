f1798b41398acad95c4477cfdd81f8fd7936d502
Francisco Rossi <65981860+FranRossi@users.noreply.github.com>
2022-05-31 18:21:14 -0300

    Refactors endpoints to reduce functions size

--- src/voter_api/controllers/voter.go
+++ src/voter_api/controllers/voter.go
@@ -6,6 +6,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	domain2 "voter_api/domain/user"
 	domain "voter_api/domain/vote"
 	"voter_api/logic"
 	proto "voter_api/proto/authService"
@@ -32,13 +33,9 @@ func RegisterServicesServer(grpcServer *grpc.Server, jwtManager *jwt.Manager) {
 }
 
 func (server *AuthServer) Login(ctx context.Context, request *proto.LoginRequest) (*proto.LoginResponse, error) {
-	user, err := logic.FindVoter(request.GetId())
+	user, err := checkVoter(request.GetId(), request.GetPassword())
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "cannot find user: %v", err)
-	}
-
-	if user == nil || !logic.IsCorrectPassword(user, request.GetPassword()) {
-		return nil, status.Errorf(codes.NotFound, "incorrect username/password")
+		return nil, err
 	}
 
 	token, err := server.jwtManager.Generate(user.Username, user.Id, user.Role)
@@ -51,9 +48,39 @@ func (server *AuthServer) Login(ctx context.Context, request *proto.LoginRequest
 }
 
 func (newVote *VoterServer) Vote(ctx context.Context, req *pb.VoteRequest) (*pb.VoteReply, error) {
-	user, err := logic.FindVoter(req.GetId())
+	voteModel, username, err := checkVote(req)
+	if err != nil {
+		return nil, err
+	}
+	err = logic.StoreVote(voteModel)
+	if err != nil {
+		return &pb.VoteReply{Message: "Error"}, status.Errorf(codes.Internal, "cannot store vote: %v", err)
+	}
+
+	message := username + " voted correctly"
+	return &pb.VoteReply{Message: message}, status.Errorf(codes.OK, "vote stored")
+
+	// rabbitmq test
+	//api_voter.sendCertificate(idVoter)
+	//api_voter.PrintCertificate()
+}
+
+func checkVoter(id, password string) (*domain2.User, error) {
+	user, err := logic.FindVoter(id)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "cannot find user: %v", err)
+	}
+
+	if user == nil || !logic.IsCorrectPassword(user, password) {
+		return nil, status.Errorf(codes.NotFound, "incorrect username/password")
+	}
+	return user, nil
+}
+
+func checkVote(req *pb.VoteRequest) (*domain.VoteModel, string, error) {
+	user, err := logic.FindVoter(req.GetId())
+	if err != nil {
+		return nil, "", status.Errorf(codes.Internal, "cannot find user: %v", err)
 	}
 	voteModel := &domain.VoteModel{
 		Id:              req.GetId(),
@@ -63,15 +90,5 @@ func (newVote *VoterServer) Vote(ctx context.Context, req *pb.VoteRequest) (*pb.
 		Candidate:       req.GetCandidate(),
 		PoliticalParty:  req.GetPoliticalParty(),
 	}
-	err = logic.StoreVote(voteModel)
-	if err != nil {
-		return &pb.VoteReply{Message: "Error"}, status.Errorf(codes.Internal, "cannot store vote: %v", err)
-	}
-
-	message := user.Username + " voted correctly"
-	return &pb.VoteReply{Message: message}, status.Errorf(codes.OK, "vote stored")
-
-	// rabbitmq test
-	//api_voter.sendCertificate(idVoter)
-	//api_voter.PrintCertificate()
+	return voteModel, user.Username, nil
 }
